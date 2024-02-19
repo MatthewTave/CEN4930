3b04669f7ca09bf17738641887f71f7d1e70a888
Matthew Tave <tavematthew@gmail.com>
2024-02-18 19:08:00 -0500

    Path Search Template Setup

--- Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.cpp
+++ Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.cpp
@@ -1,7 +1,44 @@
+#include "PathSearch.h"
+using namespace std;
+
 namespace ufl_cap4053
 {
 	namespace searches
 	{
-		// CLASS DEFINITION GOES HERE
+		PathSearch::PathSearch() {
+			return;
+		}
+
+		PathSearch::~PathSearch() {
+			return;
+		}
+
+		void PathSearch::load(TileMap* _tileMap) {
+			return;
+		}
+
+		void PathSearch::initialize(int startRow, int startCol, int goalRow, int goalCol) {
+			return;
+		}
+
+		void PathSearch::update(long timeslice) {
+			return;
+		}
+
+		void PathSearch::shutdown() {
+			return;
+		}
+
+		void PathSearch::unload() {
+			return;
+		}
+
+		bool PathSearch::isDone() const {
+			return false;
+		}
+
+		vector<Tile const*> const PathSearch::getSolution() const {
+			return vector<Tile const*>();
+		}
 	}
 }  // close namespace ufl_cap4053::searches
