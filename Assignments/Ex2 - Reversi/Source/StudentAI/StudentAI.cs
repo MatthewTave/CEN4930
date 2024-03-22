dc9b9a0fb56e097a9e77e843b3fe0c993560d99f
Matthew Tave <tavematthew@gmail.com>
2024-03-21 23:52:00 -0400

    Implemented Ex2

--- Assignments/Ex2 - Reversi/Source/StudentAI/StudentAI.cs
+++ Assignments/Ex2 - Reversi/Source/StudentAI/StudentAI.cs
@@ -1,13 +1,65 @@
 using GameAI.GamePlaying.Core;
+using System;
+using System.Runtime.Remoting.Messaging;
 
 namespace GameAI.GamePlaying
 {
     public class StudentAI : Behavior
     {
-        // TODO: Methods go here
-        public ComputerMove Run(int _color, Board _board, int _lookAheadDepth)
+        public StudentAI() { }
+
+        public ComputerMove Run(int color, Board board, int lookAheadDepth)
         {
-            throw new System.NotImplementedException();
+            int I = -1, J = -1;
+            int maxEval = int.MinValue;
+
+            for (int i = 0; i < Board.Height; i++) 
+                for (int j = 0; j < Board.Width; j++)
+                {
+                    if(!board.IsValidMove(color, i, j)) continue;
+
+                    Board futureBoard = new Board(board);
+                    futureBoard.MakeMove(color, i, j);
+                    int predEval = MiniMax(futureBoard.HasAnyValidMove(color * -1) ? color * -1 : color, futureBoard, lookAheadDepth);
+
+                    if(predEval * color > maxEval)
+                    {
+                        maxEval = predEval * color;
+                        I = i;
+                        J = j;
+                    }
+
+                }
+
+            return new ComputerMove(I, J);
+        }
+
+        private int MiniMax(int color, Board board, int lookAheadDepth)
+        {
+            if(lookAheadDepth == 0 || board.IsTerminalState()) return Eval(board);
+            int maxEval = int.MinValue;
+
+            for (int i = 0; i < Board.Height; i++)
+                for (int j = 0; j < Board.Width; j++)
+                {
+                    if (!board.IsValidMove(color, i, j)) continue;
+
+                    Board futureBoard = new Board(board);
+                    futureBoard.MakeMove(color, i, j);
+                    int predEval = MiniMax(futureBoard.HasAnyValidMove(color * -1) ? color * -1 : color, futureBoard, lookAheadDepth - 1);
+
+                    if (predEval * color > maxEval)
+                        maxEval = predEval * color;
+                }
+
+            return maxEval * color;
+        }
+
+        // Calculates the Eval Score of a given board 
+        private int Eval(Board board) {
+            int ret = 0;
+            for (int i = 0; i < Board.Height; i++) for (int j = 0; j < Board.Width; j++) ret += board.GetTile(i, j) * (i == 0 || j == 0 || i == Board.Height - 1 || j == Board.Width - 1 ? 10 : 1) * ((i == 0 && (j == 0 || j == Board.Width - 1)) || (i == Board.Height - 1 && (j == 0 || j == Board.Width - 1)) ? 10 : 1);
+            return ret + (board.IsTerminalState() && board.Score != 0 ? (board.Score > 0 ? 1 : -1) * 10000 : 0);
         }
     }
 }
