c0eeed4b2a7b3bbb45a02675aacc51ee4d9aff54
Matthew Tave <tavematthew@gmail.com>
2024-04-18 16:55:17 -0400

    Ex1 Implementation

--- Assignments/Ex1 - Flocking/Source/StudentAI/Flock.cs
+++ Assignments/Ex1 - Flocking/Source/StudentAI/Flock.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 using AI.SteeringBehaviors.Core;
 
 namespace AI.SteeringBehaviors.StudentAI
@@ -13,11 +15,106 @@ namespace AI.SteeringBehaviors.StudentAI
         protected Vector3 AverageForward { get; set; }
         public float FlockRadius { get; set; }
 
-        #region TODO
         public virtual void Update(float deltaTime)
         {
-            // Update goes here
+            this.AverageForward = get_avg_forward(this.Boids);
+            this.AveragePosition = get_avg_position(this.Boids);
+
+            foreach(MovingObject boid in this.Boids)
+            {
+                Vector3 accel = calc_alignment_accel(boid);
+                accel += calc_cohesion_accel(boid);
+                accel += calc_separation_accel(boid);
+
+                accel *= boid.MaxSpeed * deltaTime;
+
+                boid.Velocity += accel;
+
+                if(boid.Velocity.Length > boid.MaxSpeed)
+                {
+                    boid.Velocity.Normalize();
+                    boid.Velocity *= boid.MaxSpeed;
+                }
+
+                boid.Update(deltaTime);
+            }
         }
-        #endregion
+
+        private Vector3 get_avg_forward(List<MovingObject> objs)
+        {
+            Vector3 fwd = get_avg(objs.Select(obj => obj.Velocity).ToList());
+            fwd.Normalize();
+
+            return fwd;
+        }
+
+        private Vector3 get_avg_position(List<MovingObject> objs)
+        {
+            return get_avg(objs.Select(obj => obj.Position).ToList());
+        }
+
+        private Vector3 get_avg(List<Vector3> vecs)
+        {
+            Vector3 avg = new Vector3(0, 0, 0);
+
+            foreach (var vecsItem in vecs)
+            {
+                avg += vecsItem;
+            }
+
+            avg.X /= vecs.Count;
+            avg.Y /= vecs.Count;
+            avg.Z /= vecs.Count;
+
+            return avg;
+        }
+
+        private Vector3 calc_alignment_accel(MovingObject boid)
+        {
+            Vector3 accel = this.AverageForward;
+
+            if(accel.Length > 1)
+                accel.Normalize();
+
+            return accel * this.AlignmentStrength;
+        }
+
+        private Vector3 calc_cohesion_accel(MovingObject boid)
+        {
+            Vector3 accel = this.AveragePosition - boid.Position;
+            float distance = accel.Length;
+            accel.Normalize();
+
+            if(distance < this.FlockRadius) 
+                accel = accel * (distance / this.FlockRadius);
+
+            return accel * this.CohesionStrength;
+        }
+
+        private Vector3 calc_separation_accel(MovingObject boid)
+        {
+            Vector3 sum = new Vector3(0, 0, 0);
+
+            foreach(MovingObject sibling in this.Boids)
+            {
+                if (sibling == boid) continue;
+
+                Vector3 accel = boid.Position - sibling.Position;
+                float distance = accel.Length;
+                float safeDistance = boid.SafeRadius + sibling.SafeRadius;
+
+                if(distance < safeDistance)
+                {
+                    accel.Normalize();
+                    accel *= (safeDistance - distance)/safeDistance;
+                    sum += accel;
+                }
+            }
+
+            if(sum.Length > 1) sum.Normalize();
+
+            return sum * this.SeparationStrength;
+        }
+        
     }
 }
